48a5481d76384131e9be4cc22f0a42f1bae97549
RyzenG <Jongg1999@gmail.com>
2025-09-19 19:55:59 -0500

    Delete server directory

--- server/middleware/requireAuth.js
+++
@@ -1,8 +0,0 @@
-function requireAuth(req, res, next) {
-  if (req.session && req.session.userId) {
-    return next();
-  }
-  return res.status(401).json({ error: 'Autenticación requerida' });
-}
-
-module.exports = requireAuth;
--- server/scripts/migrate.js
+++
@@ -1,4 +0,0 @@
-const { runMigrations } = require('../db/migrate');
-
-runMigrations();
-console.log('Database migrated and seeded successfully.');
